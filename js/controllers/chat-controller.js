f7d50395a0c8f092dfa59aa322bb50fd290cd335
Felipe Maciel <felipemrvieira@gmail.com>
2017-03-14 11:34:33 -0300

    ajustes no mobile. mudança no header

--- js/controllers/chat-controller.js
+++ js/controllers/chat-controller.js
@@ -16,28 +16,54 @@ angular.module('casosJuridicos').controller('ChatController', ['$location' ,'$in
   self.escondeFormDescricao = true;
   self.escondeFormEmail = true;
   self.escondeFormTelefone = true;
+  self.escondeFormVoltar = true;
 
 
+
+  // var mensagem = ["Olá, eu sou Eloisa! :)", -70,
+  // "Sei que o que te trouxe aqui não deve ser um assunto tão agradável, mas não se preocupe eu estou aqui para te ajudar! ^2000",-90,
+  // "Antes de começarmos, como posso te chamar? ^500",-50,
+  //   "formNome",-70,
+  // "Prazer!",-70,
+  // "Qual a cidade e estado que você mora?",-70,
+  // "Para selecionarmos o melhor profissional para o seu caso, selecione a área do Direito que mais se aproxima com o seu caso.",-70,
+  //   "formTipo",-70,
+  // "Certo, entendi.",-70,
+  // "Agora preciso que conte rapidamente  o seu caso jurídico. ^2500",-70,
+  //   "formDescricao",-70,
+  // "Bem delicado, mas iremos te ajudar. ^2500",-70,
+  // "Para que você seja contactado por um advogado, precisamos do seu email. ^500",-70,
+  //   "formEmail",-70,
+  // "E o telefone com DDD? ^500",-70,
+  //   "formTelefone",-70,
+  // "Certo, anotei tudinho aqui! ^1000",-70,
+  // "Irei analisar seu caso com calma e te encaminhar para o melhor advogado disponível. ^1500",-70,
+  // "Enquanto isso, verifique seu email, enviaremos algumas informações para confirmação.  ^1500",-70,
+  // "Abraços e boa sorte!",
+  //     "formVoltar",-70,
+  //
+  // "fim", 8,
+  // ];
 
   var mensagem = ["Olá, eu sou Eloisa! :)", -70,
-  "Sei que o que te trouxe aqui não deve ser um assunto tão agradável, mas não se preocupe eu estou aqui para te ajudar! ^2000",-90,
-  "Antes de começarmos, como posso te chamar? ^500",-50,
+  "Sei que o que te trouxe aqui não deve ser um assunto tão agradável, mas não se preocupe eu estou aqui para te ajudar! ",-90,
+  "Antes de começarmos, como posso te chamar?",-50,
     "formNome",-70,
   "Prazer!",-70,
   "Qual a cidade e estado que você mora?",-70,
   "Para selecionarmos o melhor profissional para o seu caso, selecione a área do Direito que mais se aproxima com o seu caso.",-70,
     "formTipo",-70,
   "Certo, entendi.",-70,
-  "Agora preciso que conte rapidamente  o seu caso jurídico. ^2500",-70,
+  "Agora preciso que conte rapidamente  o seu caso jurídico.",-70,
     "formDescricao",-70,
-  "Bem delicado, mas iremos te ajudar. ^2500",-70,
-  "Para que você seja contactado por um advogado, precisamos do seu email. ^500",-70,
+  "Bem delicado, mas iremos te ajudar.",-70,
+  "Para que você seja contactado por um advogado, precisamos do seu email. ",-70,
     "formEmail",-70,
-  "E o telefone com DDD? ^500",-70,
+  "E o telefone com DDD?",-70,
     "formTelefone",-70,
-  "Certo, anotei tudinho aqui! ^1000",-70,
-  "Irei analisar seu caso com calma e te encaminhar para o melhor advogado disponível. ^1500",-70,
-  "Enquanto isso, verifique seu email, enviaremos algumas informações para confirmação.  ^1500",-70,
+  "Certo, anotei tudinho aqui!",-70,
+  "Irei analisar seu caso com calma e te encaminhar para o melhor advogado disponível.",-70,
+  "Enquanto isso, verifique seu email, enviaremos algumas informações para confirmação.",-70,
   "Abraços e boa sorte!",
       "formVoltar",-70,
 
@@ -67,20 +93,14 @@ angular.module('casosJuridicos').controller('ChatController', ['$location' ,'$in
   //     });
   // });
 
-  var intro = function(){
+  $(function(){
     $interval(function(){
       $("#intro").addClass("fadeOutUpBig");
       $("#avatar-eloisa").removeClass("hide");
       $("#avatar-eloisa").addClass("bounceInUp");
       enviaMsg();
-    }, 5000, 1);
-  }
-
-  $(function(){
-    intro();
+    }, 5, 1);
   });
-
-
 
 
     var enviaMsg = function(){
@@ -93,15 +113,15 @@ angular.module('casosJuridicos').controller('ChatController', ['$location' ,'$in
 
 
     function digita(mensagem, tempo){
-      $(document).scrollTop(1000);
-      $('#txt:last-child').typed({
-        strings: [mensagem],
-        typeSpeed: tempo,
-        callback: function() {
-          enviaMsg();
-        }
-      });
-      $(document).scrollTop(1000);
+      // $('#txt:last-child').typed({
+      //   strings: [mensagem],
+      //   typeSpeed: tempo,
+      //   callback: function() {
+      //     enviaMsg();
+      //   }
+      // });
+      $('#txt:last-child').text(mensagem);
+      enviaMsg();
     };
 
     function validaMsg(){
@@ -109,37 +129,43 @@ angular.module('casosJuridicos').controller('ChatController', ['$location' ,'$in
       switch (mensagem[i]) {
         case "fim":
           $interval.cancel(enviaMsg);
+          $(document).scrollTop(1000);
           break;
         case "formNome":
           self.escondeForm = false;
           $interval.cancel(enviaMsg);
-          $(document).scrollTop(1000);
+          $(document).scrollTop(10000);
           break;
         case "formTipo":
           self.escondeFormTipo = false;
           $interval.cancel(enviaMsg);
-          $(document).scrollTop(1000);
+          $(document).scrollTop(10000);
           break;
         case "formDescricao":
           self.escondeFormDescricao = false;
           $interval.cancel(enviaMsg);
-          $(document).scrollTop(1000);
+          $(document).scrollTop(10000);
           break;
         case "formEmail":
           self.escondeFormEmail = false;
           $interval.cancel(enviaMsg);
-          $(document).scrollTop(1000);
+          $(document).scrollTop(10000);
           break;
         case "formTelefone":
           self.escondeFormTelefone = false;
           $interval.cancel(enviaMsg);
-          $(document).scrollTop(1000);
+          $(document).scrollTop(10000);
+          break;
+        case "formVoltar":
+          self.escondeFormVoltar = false;
+          $interval.cancel(enviaMsg);
+          $(document).scrollTop(10000);
           break;
         default:
           $('#conversa').append(templateMensagem);
           digita(mensagem[i], mensagem[i+1]);
           i += 2;
-          $(document).scrollTop(1000);
+          $(document).scrollTop(10000);
 
       }
     }
@@ -169,6 +195,7 @@ angular.module('casosJuridicos').controller('ChatController', ['$location' ,'$in
       $('#conversa').append(templateResposta);
       $('#avatar-cliente:last-child').text(self.usuario.letra);
       digita(self.usuario.descricao, -8000);
+
     }
 
     self.enviaEmail = function(){
